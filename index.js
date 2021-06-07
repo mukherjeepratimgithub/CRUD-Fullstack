d7d7a91ab6cdb2bdb01f5d8f6dfc58c5a0df577c
Pratim Mukherjee <pratimmukherjeeiem@gmail.com>
2021-06-08 00:31:59 +0530

    starter

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express'); // import express
 const app = express(); // initialize express
 
+//Route
+app.get('/', function(req, res){
+  res.send({'Hello': 'World'})
+})
+
 // Listen on port 3000
 app.listen(3000, function() {
   console.log('listening on port 3000...');
