07d2d8c4ead2b61bacf1e2bf254fc135b2f7cf81
Pratim Mukherjee <pratimmukherjeeiem@gmail.com>
2021-06-09 01:40:10 +0530

    developing the frontend

--- script.js
+++ script.js
@@ -16,13 +16,26 @@ form.addEventListener('submit', function(e) {
 //   console.log(cityName);
 
 // make request to our API
-axios.get('http://localhost:3000/weather/?city=' + cityName)
-.then(function(response) {
-  console.log(response.data);
-})
-.catch(function(error) {
-  console.log(error);
-})
+  axios.get('http://localhost:3000/weather/?city=' + cityName)
+    .then(function(response) {
+      //console.log(response.data);
+      // select elements
+      let city = document.querySelector('.cityName');
+      let celsius = document.querySelector('.celsius');
+      let fahrenheit = document.querySelector('.fahrenheit');
+      let errorMessage = document.querySelector('.error-message');
+if (response.data.city) {
+        city.innerHTML = 'City: ' + response.data.city;
+        celsius.innerHTML = 'Temperature (C): ' + response.data['temperature (C)'];
+        fahrenheit.innerHTML = 'Temperature (F): ' + response.data['temperature (F)'];
+      } else {
+        errorMessage.innerHTML = "This city is not in our database"
+      }
+    
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
 
 // clear input
   textInput.value = '';
